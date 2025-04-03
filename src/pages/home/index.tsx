cfdd638f1c25c1c1a4bee21277fe164ff48b2f35
João Alberto Redondo <169093784+joaoalbertoredondo@users.noreply.github.com>
2025-04-03 12:17:53 -0300

    feat: finish home page css

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,61 @@
-import { Text, View } from "react-native";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native"
+import Icon from "react-native-vector-icons/AntDesign"
+
+import { styles } from "./styles"
 
 export function Home() {
   return (
-    <View>
-      <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require("../../../public/images/Logo.png")}
+          style={{ width: 200, height: 32, resizeMode: "contain" }}
+        />
+      </View>
+
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Adicione uma nova tarefa"
+          placeholderTextColor="#808080"
+        />
+
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>
+            <Icon name="pluscircleo" />
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.tasks}>
+        <View style={styles.tasksHeader}>
+          <View style={styles.taskBox}>
+            <Text style={styles.createdText}>Criadas</Text>
+            <Text style={styles.numberBox}>0</Text>
+          </View>
+
+          <View style={styles.taskBox}>
+            <Text style={styles.completedText}>Concluídas</Text>
+            <Text style={styles.numberBox}>0</Text>
+          </View>
+        </View>
+
+        <View style={styles.empty}>
+          <Image
+            source={require("../../../public/images/Clipboard.png")}
+            style={{ width: 56, height: 56, resizeMode: "contain" }}
+          />
+
+          <View>
+            <Text style={styles.emptyText1}>
+              Você ainda não tem tarefas cadastradas
+            </Text>
+            <Text style={styles.emptyText2}>
+              Crie tarefas e organize seus itens a fazer
+            </Text>
+          </View>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,7 +1,105 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from "react-native"
 
 export const styles = StyleSheet.create({
   container: {
-    backgroundColor: "",
+    flex: 1,
+    backgroundColor: "#1A1A1A",
+    alignItems: "center",
   },
-});
+
+  header: {
+    backgroundColor: "#0D0D0D",
+    height: 180,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  form: {
+    marginTop: -28,
+    flexDirection: "row",
+    width: "90%",
+    gap: 4,
+  },
+
+  input: {
+    flex: 1,
+    backgroundColor: "#262626",
+    borderRadius: 6,
+    paddingHorizontal: 16,
+  },
+
+  button: {
+    backgroundColor: "#1E6F9F",
+    padding: 18,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 52,
+    width: 52,
+  },
+
+  buttonText: {
+    color: "#fff",
+  },
+
+  tasks: {
+    marginTop: 32,
+    width: "90%",
+  },
+
+  tasksHeader: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+
+  taskBox: {
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+  },
+
+  createdText: {
+    color: "#4EA8DE",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+
+  completedText: {
+    color: "#8284FA",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+
+  numberBox: {
+    backgroundColor: "#333333",
+    color: "#D9D9D9",
+    paddingHorizontal: 8,
+    paddingVertical: 2,
+    borderRadius: 999,
+    fontWeight: "bold",
+    fontSize: 12,
+  },
+
+  empty: {
+    marginTop: 20,
+    alignItems: "center",
+    borderTopWidth: 1,
+    borderTopColor: "#333333",
+    paddingHorizontal: 20,
+    paddingVertical: 48,
+    gap: 16,
+  },
+
+  emptyText1: {
+    color: "#808080",
+    fontSize: 14,
+    fontWeight: "bold"
+  },
+
+  emptyText2: {
+    color: "#808080",
+    fontSize: 14,
+
+  },
+})
